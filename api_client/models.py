069fad485731fdd6b9dca4cfc271dc09ff0610c6
Adriana Alves Tavares <adritv@motorola.com>
2025-06-21 22:41:30 -0400

    CRUD Cliente

--- api_client/models.py
+++ api_client/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente (models.Model):
+    nome = models.CharField(max_length=100)
+    ID_Cliente = models.AutoField(primary_key=True)
+    CNH = models.CharField(max_length=20)
+    CPF = models.CharField(max_length=11)
